5d876bd7d7b304f2541a4f1ba52d78d9b045782b
Enrique W <kique10000@gmail.com>
2013-10-18 12:19:06 -0400

    added footer also did some for the geo location

---
+++ js/views/geoView.js
@@ -0,0 +1,48 @@
+/*jslint nomen: true, unparam: true, indent: 2, browser: true */
+/*global define */
+
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'handlebars',
+  'text!../../assets/templates/geobtn.html',
+], function ($, _, Backbone, H, controlsTpl) {
+
+
+  var GeoView = Backbone.View.extend({
+    el: '#geoContainer',
+    template: H.compile(controlsTpl),
+
+    events: {
+      'click .geoBtn': 'toggleActive'
+    },
+
+    initialize: function () {
+      console.log("Geo view Created");
+      this.render();
+      this.model.on('change:active', this.render, this);
+    },
+
+    toggleActive: function (e) {
+      //console.log("ToggleActive  Clicked");
+      console.log(this.model.toggleActive);
+      this.model.toggleActive();
+    },
+
+    render: function () {
+      console.log("geo view rendering");
+      var html, ctx = {};
+
+      ctx.active = this.model.get('active');
+
+      html = this.template(ctx);
+
+      this.$el.html(html);
+
+      // return this;
+    }
+  });
+
+  return GeoView;
+});
