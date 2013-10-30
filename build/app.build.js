68d4ce05c29e67de3cf481208cc21236890160f8
gogromat <gogromat@gmail.com>
2013-10-29 21:44:44 -0400

    Initial r.js build

---
+++ build/app.build.js
@@ -0,0 +1,17 @@
+({
+	appDir: "../",
+	baseUrl: "js",
+	dir: "../dist",
+	mainConfigFile: "../js/main.js",
+	name: "main",
+	
+	optimizeCss: "standard",
+	optimize: "uglify",
+
+
+	useStrict: false,
+	findNestedDependencies: true,
+	removeCombined: true,
+
+	preserveLicenseComments: true,
+})
---
+++ build/build.sh
@@ -0,0 +1,6 @@
+cd dist 
+rm -rf *
+cd ..
+r.js -o build/app.build.js
+cd dist
+rm -rf build build.txt assets/images/icon_set/old
